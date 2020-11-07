011d389956cde524af699beb4f39fe34144bfa0a
denghui <2603917357@qq.com>
2020-11-07 21:03:12 +0800

    完成了我的页面

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>我的</div>
+  <div class="my-contain">
+    <div class="box">
+      <van-image round
+        width="100"
+        height="100"
+        lazy-load
+        src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      <button>去登录</button>
+
+    </div>
+    <van-cell title="我的账户"
+      is-link>
+      <van-button round
+        size="mini">立即充值</van-button>
+    </van-cell>
+    <van-cell title="我的阅历"
+      is-link />
+    <van-cell title="充值记录"
+      is-link />
+    <van-cell title="购买记录"
+      is-link />
+    <van-cell title="单元格"
+      is-link />
+    <van-cell title="浏览历史"
+      is-link />
+    <van-cell title="我的阅读基因"
+      is-link />
+    <van-cell title="设置"
+      is-link />
+
+  </div>
 </template>
 
 <script>
@@ -9,4 +39,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.my-contain {
+  .box {
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
 </style>
